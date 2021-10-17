9bcb15eeaa4a6be8f6f3a8c38d6b22e4f34ea14a
Руслан Рогозин <wether@mail2000.ru>
2021-10-17 17:50:29 +0700

    Build bug fix

---
+++ Assets/Editor/DBMenuLevelsEditor.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(DBMenuLevels))]
+public class DBMenuLevelsEditor : Editor
+{
+    private int _numberLevel;
+    private List<string> _nameLevels;
+    private List<int> _numberLevels;
+
+    private DBMenuLevels _dataMenuLevels;
+
+    private void OnEnable()
+    {
+        _nameLevels = new List<string>();
+        _numberLevels = new List<int>();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        _nameLevels.Clear();
+        _numberLevels.Clear();
+
+        _dataMenuLevels = null;
+        string[] assetNames = AssetDatabase.FindAssets("", new[] {"Assets/UI/"});
+        foreach (string SOName in assetNames)
+        {
+            var SOpath = AssetDatabase.GUIDToAssetPath(SOName);
+            _dataMenuLevels =
+                (DBMenuLevels) AssetDatabase.LoadAssetAtPath<ScriptableObject>(SOpath);
+            ;
+        }
+
+        GUILayout.Space(30f);
+
+        if (_dataMenuLevels != null
+            && _dataMenuLevels.MenuLevels.Count > 0)
+        {
+            for (int i = 0; i < _dataMenuLevels.MenuLevels.Count; i++)
+            {
+                _nameLevels.Add(_dataMenuLevels.MenuLevels[i].name);
+                _numberLevels.Add(i);
+            }
+
+            _numberLevel = EditorGUILayout.IntPopup("Уровень", _numberLevel
+                , _nameLevels.ToArray(), _numberLevels.ToArray());
+        }
+
+        GUILayout.Space(10f);
+        if (GUILayout.Button("Создать UI"))
+        {
+            _dataMenuLevels.CreateMenuLevel(_numberLevel);
+        }
+    }
+}
--- Assets/Scripts/DBMenuLevels/DBMenuLevels.cs
+++ Assets/Scripts/DBMenuLevels/DBMenuLevels.cs
@@ -42,7 +42,7 @@ public struct DataMenuLevel
 public class DBMenuLevels : ScriptableObject
 {
     public List<DataMenuLevel> MenuLevels = new List<DataMenuLevel>();
-
+    #if (UNITY_EDITOR)
     public void CreateMenuLevel(int index)
     {
         var menuLevel = MenuLevels[index];
@@ -158,57 +158,5 @@ public class DBMenuLevels : ScriptableObject
         menuLevelNow.GetComponentInChildren<EventMenuLevel>()
             .isButtonNextinfoActive = menuLevel.isUseBigDescription;
     }
-}
-
-[CustomEditor(typeof(DBMenuLevels))]
-public class DBMenuLevelsEditor : Editor
-{
-    private int _numberLevel;
-    private List<string> _nameLevels;
-    private List<int> _numberLevels;
-
-    private DBMenuLevels _dataMenuLevels;
-
-    private void OnEnable()
-    {
-        _nameLevels = new List<string>();
-        _numberLevels = new List<int>();
-    }
-
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-
-        _nameLevels.Clear();
-        _numberLevels.Clear();
-
-        _dataMenuLevels = null;
-        string[] assetNames = AssetDatabase.FindAssets("", new[] {"Assets/UI/"});
-        foreach (string SOName in assetNames)
-        {
-            var SOpath = AssetDatabase.GUIDToAssetPath(SOName);
-            _dataMenuLevels = (DBMenuLevels) AssetDatabase.LoadAssetAtPath<ScriptableObject>(SOpath);;
-        }
-
-        GUILayout.Space(30f);
-
-        if (_dataMenuLevels != null
-            && _dataMenuLevels.MenuLevels.Count > 0)
-        {
-            for (int i = 0; i < _dataMenuLevels.MenuLevels.Count; i++)
-            {
-                _nameLevels.Add(_dataMenuLevels.MenuLevels[i].name);
-                _numberLevels.Add(i);
-            }
-
-            _numberLevel = EditorGUILayout.IntPopup("Уровень", _numberLevel
-                , _nameLevels.ToArray(), _numberLevels.ToArray());
-        }
-
-        GUILayout.Space(10f);
-        if (GUILayout.Button("Создать UI"))
-        {
-            _dataMenuLevels.CreateMenuLevel(_numberLevel);
-        }
-    }
+    #endif
 }
